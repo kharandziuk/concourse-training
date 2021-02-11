efb8ccdc6bf366944eb98566477da87ccdc49d7b
Max Kharandziuk <max.kharandziuk@gmail.com>
2021-02-11 21:44:29 +0100

    add test job

--- tasks.py
+++ tasks.py
@@ -13,10 +13,20 @@ def server(c):
 def login(c):
     c.run(f'fly login -t {CI_NAME} -u test -p test -c http://localhost:8080')
 
+@task
+def execute(c):
+    c.run(f"fly -t {CI_NAME} execute -i repo=. --config ci/test.yml")
+
+@task
+def test(c):
+    c.run('python test.py')
+
 ns = Collection()
+ns.add_task(test)
 
 ci = Collection('ci')
 ci.add_task(server)
 ci.add_task(login)
+ci.add_task(execute)
 
 ns.add_collection(ci)
