11283247ad27ebaf2b08c07bb09c123d534ea5b4
Max Kharandziuk <max.kharandziuk@gmail.com>
2021-02-13 23:30:50 +0100

    add basic flask app with tests

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-def sum(arr):
-    res = 0
-    for x in arr:
-        res += x
-    return res
+import os
+
+from flask import Flask
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+APP_PORT = os.environ.get("APP_PORT", 80)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Hello!"
 
 
 if __name__ == "__main__":
-    print(sum(1, 2))
+    app.run(host="0.0.0.0", port=APP_PORT)
